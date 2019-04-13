6aaca975c6fcf5e6915de9c58fd438f0bbf95625
Fortyseven <fortyseven@gmail.com>
2019-04-13 06:41:46 -0400

    Cleaned up 'usings'

--- Binalysis/src/UIComponents/DataDrawer.cs
+++ Binalysis/src/UIComponents/DataDrawer.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace Binalysis
